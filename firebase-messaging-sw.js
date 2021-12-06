0ef75f5abf407208fa7f7b1fdaccff7112fc7781
EduardoAlfredoGallegos <eduardoalgc@hotmail.com>
2021-12-06 17:56:17 -0600

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,19 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
-importScripts("https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.6.0/firebase-messaging.js"
-);
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-  apiKey: "AIzaSyBmArX8o1ObjMjXuovhSMEG97CjEwFLM9o",
-  authDomain: "learningtogetherdeploy.firebaseapp.com",
-  projectId: "learningtogetherdeploy",
-  storageBucket: "learningtogetherdeploy.appspot.com",
-  messagingSenderId: "880408318990",
-  appId: "1:880408318990:web:0e9a84f8c88b953e7ad85f"
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyARb3fUE70QrjRYvHyCoqw-mSYKQ1AzgOk",
+  authDomain: "learningtogether-faae4.firebaseapp.com",
+  projectId: "learningtogether-faae4",
+  storageBucket: "learningtogether-faae4.appspot.com",
+  messagingSenderId: "56880993418",
+  appId: "1:56880993418:web:1781c04d5e16b4baa88973",
+  measurementId: "G-XM85CRG2EV",
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
+const messaging = getMessaging(firebaseApp);
